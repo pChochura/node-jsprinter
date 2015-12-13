8da120fedecb4bdc634a003aee07bfc9a394f8ab
Hunter Morgan <automaticgiant@gmail.com>
2015-12-13 12:21:17 -0500

    comment, todo, format

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 #!/usr/bin/env node
 exports.init = function () {}
 "use strict";
-var Printer = require('ipp-printer');
+
+// track chained makes temp cleanup at shutdown
 //var temp = require('temp').track();
+
+// joblist
 var jobs = [];
+
+// ipp-printer configuration
+var Printer = require('ipp-printer');
 var printer_conf = {name: 'jsprinter', port: 40023};
-var printer_url = 'ipp://localhost:' + printer_conf.port + '/printers/' + printer_conf.name;
 var printer = new Printer(printer_conf);
+
+// for display
+var printer_url = 'http://localhost:' + printer_conf.port + '/printers/' +
+                  printer_conf.name;
+
+// register incoming job callback
 printer.on('job', function (job) {
-//    console.log(job);
-//    job.stream = temp.createWriteStream();
+  // TODO: insert debug switch for incoming job printing
+  //  console.log(job);
+
+  // TODO: maybe switch to wrapper object to cache detected file type.
+  //       alternative is to check every time we need to know, but don't know
+  //       how that would interact with it getting shunted to a temp file.
+  //       maybe would replace job object with temp file handle object with only
+  //       display information stored in memory. not sure how to keep job object
+  //       and empty buffer.
+
+  // TODO: set up temp file write
+  //  job.stream = temp.createWriteStream();
+  //  job.pipe(job.stream);
+
+    // add to joblist
     jobs.push(job);
-//   job.pipe(job.stream);
 });
-//console.log(printer.server.address().port);
 
 var express = require('express');
 var app = express();
@@ -51,4 +73,5 @@ app.get('/job/:id', function (req, res) {
 
 var joblist_port = 40024;
 app.listen(joblist_port);
-console.log('started with printer at ' + printer_url + ' and joblist at http://localhost:' + joblist_port);
+console.log('started printer at ' + printer_url);
+console.log('started joblist at http://localhost:' + joblist_port);
