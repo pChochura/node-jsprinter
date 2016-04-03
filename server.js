99d65f50cbe12ad8d723566e99a46dfd082f6dc4
Hunter Morgan <automaticgiant@gmail.com>
2016-04-03 14:59:43 -0400

    wip

--- server.js
+++ server.js
@@ -1,38 +1,30 @@
 #!/usr/bin/env node
-exports.init = function () {}
 "use strict";
 
+// TODO: change to remove assumed source - use different sources/parts of app
 var debug = require('debug')('jsprinter');
-
-// track chained makes temp cleanup at shutdown
-//var temp = require('temp').track();
 
 // joblist
 var jobs = [];
 
 // ipp-printer configuration
 var Printer = require('ipp-printer');
-var printer_conf = {name: 'jsprinter', port: 40023};
+var printer_conf = {name: 'jsprinter', port: 40023 || env.IPP_PORT, zeroconf:
+  false};
 var printer = new Printer(printer_conf);
 
 // for display
 var printer_url = 'http://localhost:' + printer_conf.port + '/printers/' +
                   printer_conf.name;
+// TODO: add to about route with app info
+
 
 // register incoming job callback
 printer.on('job', function (job) {
   // TODO: insert debug switch for incoming job printing
   //  console.log(job);
 
-  // TODO: maybe switch to wrapper object to cache detected file type.
-  //       alternative is to check every time we need to know, but don't know
-  //       how that would interact with it getting shunted to a temp file.
-  //       maybe would replace job object with temp file handle object with only
-  //       display information stored in memory. not sure how to keep job object
-  //       and empty buffer.
-
   // TODO: set up temp file write
-  //  job.stream = temp.createWriteStream();
   //  job.pipe(job.stream);
 
     // add to joblist
@@ -45,23 +37,19 @@ var app = express();
 
 // joblist index route
 app.get('/', function (req, res) {
-    if (req.accepts('text/html')) {
-        var response = 'printer is at ' + printer_url + '</br>';
-        response += 'jobs:</br><ul>';
-        jobs.forEach(function (current) {
-            response += '<li><a href=/job/' + current.id + '>';
-            response += current.id + ':' + current.name;
-            response += '</a> ';
-            response += '<a href=/job/' + current.id + '?delete>';
-            response += 'forget job';
-            response += '</a>';
-            response += '</li>';
-        });
-        response += '</ul>';
-        res.send(response);
-    } else {
-        res.status(500).send('Not Implemented Yet');
-    }
+    var response = 'printer is at ' + printer_url + '</br>';
+    response += 'jobs:</br><ul>';
+    jobs.forEach(function (current) {
+        response += '<li><a href=/job/' + current.id + '>';
+        response += current.id + ':' + current.name;
+        response += '</a> ';
+        response += '<a href=/job/' + current.id + '?delete>';
+        response += 'forget job';
+        response += '</a>';
+        response += '</li>';
+    });
+    response += '</ul>';
+    res.send(response);
 });
 
 // job access route
@@ -87,7 +75,7 @@ app.get('/job/:id', function (req, res) {
     }
 );
 
-var joblist_port = 40024;
+var joblist_port = 40024 || env.JOBLIST_PORT;
 app.listen(joblist_port);
 console.log('started printer at ' + printer_url);
 console.log('started joblist at http://localhost:' + joblist_port);
